9b8531bffc089e93edbdcdbee4995f4faa6ad32c
mig1998 <miguelaras@hotmail.com>
2022-04-30 18:21:21 -0300

    finished!

--- aplicacao-live/routes/index.js
+++ aplicacao-live/routes/index.js
@@ -1,13 +1,32 @@
 var express = require('express');
 var router = express.Router();
 
-const axios = require('axios')
+const axios = require('axios');
+
+
+
+
+
+var id_usuario = "";
 
 
 
 
 
 
+
+axios.post('http://138.68.7.94:85/busca_comentarios', {})
+
+  .then(res => {
+
+    console.log(`statusCode: ${res.status}`);
+
+    coments = res.data
+  })
+
+  .catch(error => {
+    res.send(error, 'Falha no Cadastro, você deixou algum campo')
+  });
 
 
 
@@ -15,20 +34,11 @@ const axios = require('axios')
 
 /* GET home page. */
 router.get('/cadastrar', function (req, res, next) {
-  var usuario_id = 0;
 
-  res.cookie("email", `${req.query.email}`, {
-    maxAge: 5000,
-    httpOnly: true
-  });
-  res.cookie("nome", `${req.query.nome}`, {
-    maxAge: 5000,
-    httpOnly: true
-  });
-  res.cookie("usuario_id", usuario_id, {
-    maxAge: 5000,
-    httpOnly: true
-  });
+
+  res.clearCookie('id_usuario')
+
+
 
   axios.post('http://138.68.7.94:85/cadastro', {
     nome: req.query.nome,
@@ -36,15 +46,16 @@ router.get('/cadastrar', function (req, res, next) {
   })
 
     .then(res => {
-      usuario_id = res.data.id;
+      email = req.query.email
+      nome = req.query.nome
+      id_usuario = res.data.id;
+
 
       console.log(`statusCode: ${res.status}`);
       console.log(res);
 
 
-      router.get('/del-cookie')
-      req.query.nome = ''
-      req.query.email = ''
+
     })
 
     .catch(error => {
@@ -57,26 +68,23 @@ router.get('/cadastrar', function (req, res, next) {
 
 
 
-
-
-
 // comentar
 router.get('/comentar', function (req, res, next) {
 
-  res.cookie("comentario", `${req.query.comentario}`, {
+  res.cookie("id_usuario", id_usuario, {
     maxAge: 5000,
     httpOnly: true
   });
 
+console.log(req.query.comentario)
+
 
   axios.post('http://138.68.7.94:85/add_comentario', {
-    id_usuario: 83,
+    id_usuario: req.cookies.id_usuario,
     comentario: req.query.comentario,
 
   })
     .then(res => {
-      usuario_id = res.data.id;
-
       console.log(`statusCode: ${res.status}`);
       console.log(res);
 
@@ -87,48 +95,13 @@ router.get('/comentar', function (req, res, next) {
     });
 
 
-
-  res.render('comentar', { title: 'Comentar' });
-});
-
-
-
-
-
-
-
-// todos os comentarios (ate 100)
-router.get('/coments', function (req, res, next) {
-
-  var coments =
-    [{
-      comentario: '',
-      data_entrada: '',
-      horario: '',
-      nome: '',
-      email: ''
-
-    }]
-
-  axios.post('http://138.68.7.94:85/busca_comentarios', {})
+    axios.post('http://138.68.7.94:85/busca_comentarios', {})
 
     .then(res => {
 
       console.log(`statusCode: ${res.status}`);
 
-      coments = [{
-        comentario: res.data.comentario,
-        data_entrada: res.data.data_entrada,
-        horario: res.data.horario,
-        nome: res.data.nome,
-        email: res.data.email
-      }]
-
-
       coments = res.data
-
-
-
     })
 
     .catch(error => {
@@ -136,43 +109,12 @@ router.get('/coments', function (req, res, next) {
     });
 
 
-  console.log(coments);
-  res.send(coments);
+
+
+  res.render('comentar', { title: 'Comentar', comentarios: coments, id: id_usuario});
 });
 
 
-
-
-router.get('/del-cookie', function (req, res, next) {
-
-  res.clearCookie('nome')
-  res.clearCookie('email')
-  router.get('/cadastrar')
-});
-
-
-
-
-
-// router.get('/set-cookie', function (req, res, next) {
-//   res.cookie("token name", "encrypted cookie string Value", {
-//     maxAge: 5000,
-//     // expires:new Date(''),
-//     httpOnly: true,
-//     // secure:true ssl
-//   });
-//   res.send("worth!");
-// });
-
-// router.get('/get-cookie', function (req, res, next) {
-//   res.send(req.cookies);
-// });
-
-
-
-// router.get('/del-cookie', function (req, res, next) {
-//   res.clearCookie('token name')
-// });
 
 
 
